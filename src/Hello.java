5f3287ec82bce302a9926b0611dc516c7e95c1ce
Benjamin J. Broad <benjamin.broad@colorado.edu>
2019-04-13 15:34:38 -0600

    basics of java

---
+++ src/Hello.java
@@ -0,0 +1,19 @@
+public class Hello {
+
+    public static void main (String[] args) {
+
+        System.out.println("Hello, Ben!");
+
+        int myFirstNumber = 5 + 10;
+        int mySecondNumber = 25;
+        int myThirdNumber = 6;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        int newValue = 1000 - myTotal;
+
+        System.out.println(newValue);
+
+    }
+
+}
